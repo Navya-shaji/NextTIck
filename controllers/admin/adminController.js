87eb4e2c791ae73f6fb76eda781eb1776fd6597b
Navyacs-crypto <navyacshaji12@gmail.com>
2025-08-06 21:51:43 +0530

    controller formated

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -24,7 +24,7 @@ const loadLogin = (req, res) => {
     }
     res.render("adminLogin", { message: null })
 }
-
+console.log(loadLogin)
 
 //login..........................................................................................................
 
